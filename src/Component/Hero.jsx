6a298504b6d52dfee485a124f96ac30b1307d04a
ABHCH <chaturvedia435@gmail.com>
2023-12-01 22:47:24 +0530

    Change

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <SideBar />
       <div
         id="about"
-        className="text-black h-[70vh] mt-[80px] w-[80%] mx-auto flex justify-center items-center"
+        className="text-black h-[70vh] mt-[80px] w-[80%]  mx-auto flex justify-center items-center"
       >
         <div className="left font-Poppins max-md:text-center w-full">
           <h1 className="text-4xl font-Poppins font-bold">Hi, I am</h1>
@@ -25,9 +25,9 @@ const Hero = () => {
               />
             </span>
           </h1>
-          <div className="w-[80%] mx-sm:mx-auto">
-            <p className="text-[5px] mt-4">{Bio.description}</p>
-          </div>
+          <p className="w-[80%] max-sm:w-[100%] text-[5px] mt-4">
+            {Bio.description}
+          </p>
           <a href="./abhi(1).pdf" download>
             <button className="resumebtn mt-4 border px-5 w-[15rem] py-3 font-bold font-Poppins">
               Get RESUME
