d8f7ceaf58ec47c045c4f0efaa4afd582582ef38
ABHCH <chaturvedia435@gmail.com>
2023-12-01 22:54:56 +0530

    improve ui

--- src/Component/ProjectModal.jsx
+++ src/Component/ProjectModal.jsx
@@ -9,8 +9,8 @@ const ProjectModal = ({ closeModal }) => {
     };
   }, []);
   return (
-    <div className="fixed z-1000 w-full h-full top-0 left-0 bottom-0 right-0 overflow-y-hidden  bg-transparentColor  ">
-      <div className="modal-container bg-[#171721] h-[80vh] py-2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  w-[50%]">
+    <div className="fixed z-1000 w-full h-full top-0 left-0 bottom-0 right-0 overflow-y-hidden max-sm:overflow-y-hidden max-md:overflow-y-hidden  bg-transparentColor  ">
+      <div className="modal-container bg-[#171721] h-[80vh] py-2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-sm:w-full max-md-w-full px-4 w-[50%]">
         <ImCross
           onClick={closeModal}
           className="text-white cursor-pointer absolute right-0 "
